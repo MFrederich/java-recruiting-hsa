8031e674c9986bcb0f1f08d6d432e0b07aa04ec1
marcelo aceituno <marceloaceituno.accenture@itau.cl>
2020-01-17 17:18:56 -0300

    test wiremock

--- src/testIntegration/java/com/concrete/desafio/coupons/CouponIntegrationTest.java
+++ src/testIntegration/java/com/concrete/desafio/coupons/CouponIntegrationTest.java
@@ -1,25 +1,54 @@
 package com.concrete.desafio.coupons;
 
+import com.concrete.desafio.coupons.api.Coupon;
+import com.concrete.desafio.coupons.api.CouponRepository;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@AutoConfigureWireMock()
 public class CouponIntegrationTest {
 
-  @Autowired private CouponController couponController;
+  private WireMockServer wireMockServer;
+  @Autowired private CouponRepository couponRepository;
+  private CouponController couponController;
+
+  @BeforeAll
+  private void setup() {
+    this.wireMockServer = new WireMockServer();
+    this.wireMockServer.start();
+
+    final CouponService couponService = new CouponDefaultService(couponRepository);
+    couponController = new CouponController(couponService);
+  }
+
+  @AfterEach
+  void stopWireMockServer() {
+    this.wireMockServer.stop();
+  }
 
   @Test
   public void contextLoads() throws Exception {
-    assertThat(this.couponController.getCoupons().getBody()).isEqualTo(null);
+    wireMockServer.stubFor(
+        get(urlEqualTo("/coupons"))
+            .willReturn(
+                aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-Type", "application/json")
+                    .withBodyFile("")));
+    List<Coupon> coupons = couponController.getCoupons().getBody();
   }
 }
