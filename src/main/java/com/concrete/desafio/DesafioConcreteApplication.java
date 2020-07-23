89bddf8b69eb338aa8705e2508dfd7af6adb8e63
marcelo aceituno <frederich.alex@gmail.com>
2020-07-22 23:28:54 -0400

    implementando circuit breaker- falta probar

--- src/main/java/com/concrete/desafio/DesafioConcreteApplication.java
+++ src/main/java/com/concrete/desafio/DesafioConcreteApplication.java
@@ -2,10 +2,12 @@ package com.concrete.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients()
+@EnableCircuitBreaker
 public class DesafioConcreteApplication {
 
   public static void main(final String[] args) {
